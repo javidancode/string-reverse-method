9f2a7f9b2ceed0de08222f46282e7e8a71b680fd
javidancode <javidanig@code.edu.az>
2022-06-22 03:14:09 +0400

    Added string reverse method table

---
+++ homework reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace homework_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "C", "a", "v", "i", "d", "a", "n" };
+            Word(name);
+
+        }
+
+        static void Word(string[] name)
+        {
+            int count = 0;
+
+            for (int i = 6; i < name.Length; i--)
+            {
+                count++;
+                Console.WriteLine(name[i]);
+            }
+
+        }
+
+
+
+
+    }
+}
